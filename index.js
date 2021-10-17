ca1673ea381eda503ffb13d6e9558524a55aa591
Rohit R <rohitsmudge190@gmail.com>
2021-10-17 21:33:35 +0530

    configured the views and set the view engine

--- index.js
+++ index.js
@@ -2,10 +2,12 @@ const express = require('express');
 const port = 8000;
 
 const app = express();
-
 //use express router
 app.use('/', require('./routes/index'));
 
+//setting up views of the app
+app.set('view engine', 'ejs');
+app.set('views', './views');
 
 app.listen(port, (err) => {
     if(err) {
@@ -14,3 +16,5 @@ app.listen(port, (err) => {
     }
     console.log(`Server running on port: ${port}`);
 });
+// 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 -pat
+// vscode://vscode.github-authentication/did-authenticate?windowid=1&code=348ee6e331c90d4342ae&state=bdf5e338-80ed-4284-acf5-3757741ca709
